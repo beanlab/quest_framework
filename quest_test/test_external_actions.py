8921d7adc951dc7e1332eee772379e57eb6da530
gbean <brazilbean@gmail.com>
2023-07-15 20:04:42 -0600

    failflow test passing

--- quest_test/test_external_actions.py
+++ quest_test/test_external_actions.py
@@ -98,15 +98,6 @@ async def test_external_queue():
     assert await workflow == [7, 8, 9]
 
 
-async def one_of(*coro):
-    tasks = [asyncio.create_task(co) for co in coro]
-    done, pending = asyncio.wait(tasks)
-    for i in range(len(tasks)):
-        if tasks[i] in done:
-            return i, done
-    raise Exception('Unreachable code was reached')
-
-
 @task
 async def get_foos(identity, foo_values: list):
     async with queue('foo', identity) as foos:
@@ -163,76 +154,6 @@ async def test_queue_tasks():
     assert await workflow == [1, 2, 3, 4, 5]
 
 
-"""
-
-
-@task
-async def get_sequence(ident: str, q: Queue):
-    async with state('received', identity=ident) as receceived:
-        value1 = await q.pop()
-        await receceived.set(value1)
-        value2 = await q.pop()
-        await receceived.set(value2)
-        return value1, value2
-
-
-async def workflow2(ident1, ident2):
-    async with these([queue('items', identity=idt) for idt in [ident1, ident2]]) as qs:
-        value_tasks = [get_sequence(idt, q) for idt, q in zip([ident1, ident2], qs)]
-        values = [await t for t in value_tasks]
-
-    return values
-
-
-@pytest.mark.asyncio
-async def test_queues_tasks(tmp_path):
-    ident1 = 'first'
-    ident2 = 'second'
-    wid = '123'
-    async with get_local_workflow_manager(tmp_path, workflow2) as wm:
-        # Start workflow
-        await wm.run(wid, 'workflow2', ident1, ident2)
-
-        # Initial status
-        status = await wm.get_status(wid, identity=ident1)
-        assert status.state['received']['value'] is None
-
-        status = await wm.get_status(wid, identity=ident2)
-        assert status.state['received']['value'] is None
-
-        # Progress ident2
-        await wm.push_queue(wid, 'items', 'a', identity=ident2)
-        status = await wm.get_status(wid, identity=ident2)
-        assert status.state['received']['value'] == 'a'
-
-        status = await wm.get_status(wid, identity=ident1)
-        assert status.state['received']['value'] is None
-
-        # Progress ident2 again - closes state and queue
-        await wm.push_queue(wid, 'items', 'b', identity=ident2)
-        status = await wm.get_status(wid, identity=ident2)
-        assert 'received' not in status.state  # i.e. the 'received' state is gone now
-
-        status = await wm.get_status(wid, identity=ident1)
-        assert status.state['received']['value'] is None
-
-        # Now ident1
-        await wm.push_queue(wid, 'items', 'x', identity=ident1)
-        status = await wm.get_status(wid, identity=ident1)
-        assert status.state['received']['value'] == 'x'
-
-        await wm.push_queue(wid, 'items', 'y', identity=ident1)
-        status = await wm.get_status(wid, identity=ident1)
-        assert 'received' not in status.state
-
-        # Should now be finished
-        status = await wm.get_status(wid)
-        assert status.state['result']['value'] == [
-            ('x', 'y'),
-            ('a', 'b'),
-        ]
-
-
 @task
 async def fail():
     raise Exception('Epic Fail')
@@ -255,9 +176,12 @@ async def failflow():
 
 @pytest.mark.asyncio
 async def test_failflow(tmp_path):
-    async with get_local_workflow_manager(tmp_path, failflow) as wm:
-        try:
-            await wm.run('test', 'failflow')
-        except asyncio.CancelledError:
-            pass
-"""
+    historian = Historian(
+        'test',
+        failflow,
+        [], {}
+    )
+    try:
+        await historian.run()
+    except ExceptionGroup as ex:
+        assert ex.exceptions[0].args[0] == 'Epic Fail'
