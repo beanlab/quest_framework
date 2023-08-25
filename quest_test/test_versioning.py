6b61e35bc9fa8579102f0f54594cd1797e2e44e0
Gordon Bean <gbean@cs.byu.edu>
2023-08-25 16:14:09 -0600

    test the multi-versioning

--- quest_test/test_versioning.py
+++ quest_test/test_versioning.py
@@ -49,8 +49,7 @@ async def test_versioning():
     assert (await result) == ['foo', 'bar', 'baz2']
 
 
-CASING_V2 = '2023-08-25 upper case'
-CASING_V3 = '2023-08-25a title case'
+CASING_V2 = '2023-08-25 title case'
 
 
 @pytest.mark.asyncio
@@ -68,13 +67,16 @@ async def test_multi_versioning():
     historian.run()
     await asyncio.sleep(0.1)
     await historian.record_external_event('words', None, 'put', 'foo')
-    await historian.record_external_event('words', None, 'put', 'bar')
     await asyncio.sleep(0.01)
 
     # Application shuts down
     await historian.suspend()
 
     # New version of application is deployed
+    # Make word upper and append '2'
+    async def fix_case(word):
+        return word.upper()
+
     @version(V2)
     async def application():
         phrase = []
@@ -84,7 +86,33 @@ async def test_multi_versioning():
                 if get_version() < V2:
                     phrase.append(word)
                 else:
-                    phrase.append(word + '2')
+                    phrase.append(await fix_case(word) + '2')
+        return phrase
+
+    historian = Historian('test', application, history)
+    result = historian.run()
+    await asyncio.sleep(0.1)
+    await historian.record_external_event('words', None, 'put', 'bar')
+
+    # Yet another version of application is deployed
+    # Use title-case instead of upper
+    @version(CASING_V2)
+    async def fix_case(word: str) -> str:
+        if get_version() < CASING_V2:
+            return word.upper()
+        else:
+            return word.title()
+
+    @version(V2)
+    async def application():
+        phrase = []
+        async with queue('words', None) as words:
+            while len(phrase) < 3:
+                word = await words.get()
+                if get_version() < V2:
+                    phrase.append(word)
+                else:
+                    phrase.append(await fix_case(word) + '2')
         return phrase
 
     historian = Historian('test', application, history)
@@ -92,4 +120,4 @@ async def test_multi_versioning():
     await asyncio.sleep(0.1)
     await historian.record_external_event('words', None, 'put', 'baz')
 
-    assert (await result) == ['foo', 'bar', 'baz2']
+    assert (await result) == ['foo', 'BAR2', 'Baz2']
