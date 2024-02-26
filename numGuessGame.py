5a15cd42ea6cf6c754f8482b571226cba0eb1a3b
ABCambridge <andrewbcambridge@gmail.com>
2024-02-26 08:39:55 -0700

    complete arg set up for historian versus workflow manager. Workflow not working

--- numGuessGame.py
+++ numGuessGame.py
@@ -2,6 +2,8 @@ import asyncio
 import uuid
 import random
 from pathlib import Path
+import sys
+import shutil
 
 from src.quest import step, create_filesystem_historian
 from src.quest.external import state, queue
@@ -45,37 +47,58 @@ async def play_game():
         return message
 
 async def game_loop(*args, **kwargs):
-    print("type q to stop")
+    print("type q to quit the game.")
     while((res := await play_game()) != -1):
         print(res)
     print("Adios from the game loop!")
     return "Game loop completed"
 
+def usage():
+    print("USAGE: Please specify which method with which to run the game.")
+    print("\"-w\" to use Workflow Manager, or \"-h\" to run directly on the historian.")
+    print("Specifying \"-r\" with any other flag, or none, will refresh the history.")
+    exit(1)
+
 async def main():
     game_state = Path("game-state")
-    workflow_id = "the_guessing_game"
-    historian = create_filesystem_historian(game_state, "Guessing_Game", game_loop)
-    task = historian.run()
-    await task
-    print(f'RESULT: {task.result()}')
+    options = ["-w", "-h", "-r"]
+    args = sys.argv
+
+    if len(args) < 2:
+        usage()
+    elif args[1] not in options:
+        usage()
+
+    if "-r" in args:
+        shutil.rmtree(game_state, ignore_errors=True)
+        print("Previous JSON files have been removed.")
+
+    if(args[1] == "-h"):
+        workflow_id = "the_guessing_game"
+        historian = create_filesystem_historian(game_state, "Guessing_Game", game_loop)
+        task = historian.run()
+        await task
+        print(f'RESULT: {task.result()}')
+
+    elif(args[1] == "-w"):
+        storage = LocalFileSystemBlobStorage(game_state)
+        histories = {}
+
+        def create_history(wid: str):
+            if wid not in histories:
+                histories[wid] = PersistentHistory(wid, LocalFileSystemBlobStorage(game_state))
+            return histories[wid]
+        
+        async with WorkflowManager('number-game', storage, create_history, lambda wkflw: game_loop) as manager:
+            await manager.start_workflow('workflow', 'game1', 0)
+            print('with statement is finishing')
 
 
     # the apparent advantage of using the workflow manager is that completed workflows are forgotten
         # i.e., if I keyboard interrupt and throw a Ctrl+C exception, that terminations the workflow,
         # which means that next time I start the program, I just start fresh on a new copy of the work flow.
-    # storage = LocalFileSystemBlobStorage(game_state)
-    # histories = {}
-
-    # def create_history(wid: str):
-    #     if wid not in histories:
-    #         histories[wid] = PersistentHistory(wid, LocalFileSystemBlobStorage(game_state))
-    #     return histories[wid]
     
-    # async with WorkflowManager('number-game', storage, create_history, lambda wkflw: game_loop) as manager:
-    #     await manager.start_workflow('workflow', 'game1', 0)
-    #     print('with statement is finishing')
     
-
 if __name__ == '__main__':
     loop = asyncio.new_event_loop()
 
