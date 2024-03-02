58b473f7702e272325c16796cb715e97d02ba8c9
ABCambridge <andrewbcambridge@gmail.com>
2024-03-02 11:46:41 -0700

    part way through stubbing out test

--- temp.py
+++ temp.py
@@ -1,8 +1,10 @@
 import asyncio
 import logging
+from pathlib import Path
+import shutil
+from src.quest import step, create_filesystem_historian
 from src.quest import PersistentHistory, queue
 from src.quest.manager import WorkflowManager
-from pathlib import Path
 from src.quest.persistence import InMemoryBlobStorage, LocalFileSystemBlobStorage
 
 async def test_manager_background():
@@ -66,16 +68,33 @@ async def test_manager_background():
     print("test_manager_background() completed successfully with no assertion failures.")
 
 # TODO: this should become its own test file
+    # TODO: declare global events for setting in-between interrupts (I would expect to need at least 2)
+    # TODO: create @step that throws a KeyboardInterrupt
+
+async def createInterrupt():
+    # TODO: use a send_message type pattern (see the test_manager_background function) to tell the workflow
+        # when to throw the KeyboardInterrupt
+    pass    
+
 async def test_interrupt_handling():
+    test_state = Path("test-state-(temp)")
+    historian = create_filesystem_historian(test_state, "Interrupt_Testing", createInterrupt)
+
+
     # write the test for Ctrl+C here
         # it might be worth writing essentially the same test twice, but one using the
             # Historian directly, and the other running on top of a WorkflowManager so
             # that we can not only confirm the Historian handling it correctly, but also
-            # that the WorkflowManager will function correctly with it.  
-    # TODO: we do need to figure out how this applies to SIGTERM. 
-    pass
+            # that the WorkflowManager will function correctly with it.
+
+    
+    # clean up files after the test
+    shutil.rmtree(test_state, ignore_errors=True) 
+
+
 
 # asyncio.run(test_manager_background())
+# TODO: we do need to figure out how this applies to SIGTERM.  
 asyncio.run(test_interrupt_handling())
 
 
