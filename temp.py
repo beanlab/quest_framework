9ddb4f4868de25f2a3d4cff66243e210b11be1b8
ABCambridge <andrewbcambridge@gmail.com>
2024-03-06 10:36:27 -0700

    ontrack to have a valid test for the SIGINT handling and added to gitignore

--- temp.py
+++ temp.py
@@ -7,70 +7,14 @@ from src.quest import PersistentHistory, queue
 from src.quest.manager import WorkflowManager
 from src.quest.persistence import InMemoryBlobStorage, LocalFileSystemBlobStorage
 
-async def test_manager_background():
-    # storage = InMemoryBlobStorage() # this caused issues when the objects stored as values would disappear
-    storage = LocalFileSystemBlobStorage(Path('test-state'))
-    histories = {}
-
-    def create_history(wid: str):
-        if wid not in histories:
-            histories[wid] = PersistentHistory(wid, InMemoryBlobStorage())
-        return histories[wid]
-
-    counter_a = 0
-    counter_b = 0
-    total = 0
-
-    async def workflow(arg: int):
-        nonlocal counter_a, counter_b, total
-        total = arg
-
-        logging.info('workflow started')
-        counter_a += 1
-
-        async with queue('messages', None) as Q:
-            while True:
-                message = await Q.get()
-                logging.info(f'message received: {message}')
-                counter_b += 1
-
-                if message == 0:
-                    return
-
-                total += message
-
-    async with WorkflowManager('test-manager', storage, create_history, lambda w_type: workflow) as manager:
-        manager.start_workflow('workflow', 'wid1', True, 1)
-        await asyncio.sleep(0.1)
-        await manager.send_event('wid1', 'messages', None, 'put', 2)
-        await asyncio.sleep(0.1)
-        # Now pause the manager and all workflows
-
-    assert 'wid1' in histories
-    assert counter_a == 1
-    assert counter_b == 1
-
-    await asyncio.sleep(.1)
-
-    async with WorkflowManager('test-manager', storage, create_history, lambda w_type: workflow) as manager:
-        # At this point, all workflows should be resumed
-        await asyncio.sleep(0.1)
-        await manager.send_event('wid1', 'messages', None, 'put', 3)
-        assert manager.has_workflow('wid1')
-        await manager.send_event('wid1', 'messages', None, 'put', 0)  # i.e. end the workflow
-        await asyncio.sleep(0.1)  # workflow now finishes and removes itself
-        assert not manager.has_workflow('wid1')
-        assert total == 6
-
-    assert counter_a == 2
-    assert counter_b == 4  # 2, replay 2, 3, 0
-
-    print("test_manager_background() completed successfully with no assertion failures.")
-
-# TODO: this should become its own test file
-
 # global events to be set  by the workflow and indicate how for the workflow progressed
 three_events: list[asyncio.Event] = [asyncio.Event(), asyncio.Event(), asyncio.Event()]
+
+# directory for storage of json files
+test_state = Path("test-state-(temp)")
+
+# broadly used constant
+MAX_ITERATIONS = 3
 
 @step
 async def throwExceptionOnTrigger(current_iteration, iteration_to_trigger):
@@ -79,24 +23,21 @@ async def throwExceptionOnTrigger(current_iteration, iteration_to_trigger):
 
 async def createInterrupt(iteration_to_trigger):
     current_iteration: int = 0
-    # try:
+
     while(current_iteration < 3):
         await throwExceptionOnTrigger(current_iteration, iteration_to_trigger)
         three_events[current_iteration].set()
         current_iteration += 1
-    # except KeyboardInterrupt:
-    #     pass
 
     return current_iteration
 
-async def test_interrupt_handling():
-    test_state = Path("test-state-(temp)")
-
+async def test_interrupt_handling(iterations):
     # test against historian
     print("Testing interrupts on filesystem Historian:")
 
     # test interrupt on first iteration
     historian = create_filesystem_historian(test_state, "Interrupt_Testing", createInterrupt)
+
     try:
         task: asyncio.Task = historian.run(0)
         await task
@@ -115,26 +56,27 @@ async def test_interrupt_handling():
         # it might be worth writing essentially the same test twice, but one using the
             # Historian directly, and the other running on top of a WorkflowManager so
             # that we can not only confirm the Historian handling it correctly, but also
-            # that the WorkflowManager will function correctly with it.
-
-    
-    # clean up files after the test
-    shutil.rmtree(test_state, ignore_errors=True) 
+            # that the WorkflowManager will function correctly with it. 
 
 
-
-# asyncio.run(test_manager_background())
 # TODO: we do need to figure out how this applies to SIGTERM. 
 if __name__ == '__main__': 
+    # the below code is basically what should go in the test function since this is where we'll do assertions and restart the test routine
     loop = asyncio.new_event_loop()
     
-    try:
-        loop.run_until_complete(test_interrupt_handling())
+    iteration = 0
 
-    except Exception as ex:
-        print(f"Caught exception {ex.__class__}")
+    while(iteration < MAX_ITERATIONS):
 
-    finally:
-        loop.close()
+        try:
+            print(f"Iteration number {iteration}")
+            loop.run_until_complete(test_interrupt_handling(iteration))
+
+        except Exception as ex:
+            # print(f"Caught exception {ex.__class__}")
+            iteration = iteration + 1
+            continue
 
 
+    # clean up files after the test
+    shutil.rmtree(test_state, ignore_errors=True)
