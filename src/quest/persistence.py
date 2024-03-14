9f3922cddb76dee808cb92b5ac03e6922fe0e52d
Andrew Cambridge <andrewbcambridge@gmail.com>
2024-03-14 15:15:02 -0600

    cleaned up some code and ensured things are still working smoothly

--- src/quest/persistence.py
+++ src/quest/persistence.py
@@ -19,57 +19,15 @@ class BlobStorage(Protocol):
 
     def delete_blob(self, key: str): ...
 
-
-class PersistentHistory1(History):
-    def __init__(self, namespace: str, storage: BlobStorage):
-        self._namespace = namespace
-        self._storage = storage
-        # TODO - use linked list instead of array list
-        # master stores head/tail keys
-        # each blob is item, prev, next
-        # only need to modify blobs for altered nodes
-        # on add/delete, and rarely change master head/tail blob
-        self._items = []
-        self._keys: list[str] = []
-
-        if storage.has_blob(namespace):
-            self._keys = storage.read_blob(namespace)
-            for key in self._keys:
-                self._items.append(storage.read_blob(key))
-
-    def _get_key(self, item: EventRecord) -> str:
-        return self._namespace + '.' + md5((item['timestamp'] + item['step_id'] + item['type']).encode()).hexdigest()
-
-    def append(self, item: EventRecord):
-        self._items.append(item)
-        self._keys.append(key := self._get_key(item))
-        self._storage.write_blob(key, item)
-        self._storage.write_blob(self._namespace, self._keys)
-
-    def remove(self, item: EventRecord):
-        self._items.remove(item)
-        self._keys.remove(key := self._get_key(item))
-        self._storage.delete_blob(key)
-        self._storage.write_blob(self._namespace, self._keys)
-
-    def __iter__(self):
-        return iter(self._items)
-
-    def __reversed__(self):
-        return reversed(self._items)
-
-#------------------------------------------
 class HistoryNode():
     def __init__(self):
         self.prev: HistoryNode = None
         self.next: HistoryNode = None
         self.item = None
-
 class PersistentHistory(History):
     def __init__(self, namespace:str, storage: BlobStorage):
         self._namespace = namespace
         self._storage = storage
-        # self._keys: list[str] = []
         self._nodes: dict[str, HistoryNode] = {}
 
         self._head: HistoryNode = None
@@ -149,8 +107,6 @@ class PersistentHistory(History):
     
     def __reversed__(self):
         return self.HistoryIterator(self._tail, 'prev')
-
-#-------------------------------------------
 
 class LocalFileSystemBlobStorage(BlobStorage):
     def __init__(self, root_folder: Path):
