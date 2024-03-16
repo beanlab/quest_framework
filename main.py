8318e279b3ac1e59e50d452f82f9d072e5480a32
Andrew Cambridge <andrewbcambridge@gmail.com>
2024-03-15 20:18:33 -0600

    queue input is working with the manager and multi guess

--- main.py
+++ main.py
@@ -15,29 +15,22 @@ async def main():
 
     def get_workflow(arg: str):
         return game_loop
-    
-    historian = create_filesystem_historian(saved_state, 'demo', game_loop)
-    workflow_task = historian.run()
-    await asyncio.sleep(0.1)
-    await historian.record_external_event('guess', None, 'put', '5')
-    await asyncio.sleep(0.1)
-    await historian.record_external_event('guess', None, 'put', '10')
-    await asyncio.sleep(0.1)
-    await historian.record_external_event('guess', None, 'put', '6')
-    await asyncio.sleep(0.1)
-    await historian.record_external_event('guess', None, 'put', '25')
-    await asyncio.sleep(0.1)
 
-    await workflow_task
-    print(workflow_task.result())
-    
-    # async with create_filesystem_manager(saved_state, workflow_namespace_root, get_workflow) as manager:
-    #     workflow_1 = f'{workflow_namespace_root}-{workflow_number}'
-    #     manager.start_workflow('multi-guess', workflow_1, False, ['-w', '-r'])
-    #     await manager.send_event(workflow_1, 'guess', None, 'put', 5)
-    #     await asyncio.sleep(0.1)
-    #     result = await manager.get_workflow(workflow_1)
-    #     print(result)
+    async with create_filesystem_manager(saved_state, workflow_namespace_root, get_workflow) as manager:
+        workflow_1 = f'{workflow_namespace_root}-{workflow_number}'
+        # workflow_1 = 'demo'
+        manager.start_workflow('multi-guess', workflow_1, False, ['-w', '-r'])
+        await asyncio.sleep(0.1)
+        await manager.send_event(workflow_1, 'guess', None, 'put', 5)
+        await asyncio.sleep(0.1)
+        await manager.send_event(workflow_1, 'guess', None, 'put', 5)
+        await asyncio.sleep(0.1)
+        await manager.send_event(workflow_1, 'guess', None, 'put', 5)
+        await asyncio.sleep(0.1)
+        await manager.send_event(workflow_1, 'guess', None, 'put', 'q')
+        await asyncio.sleep(0.1)
+        result = await manager.get_workflow(workflow_1)
+        print(f'Result: {result}')
         
 if __name__ == '__main__':
     loop = asyncio.new_event_loop()
