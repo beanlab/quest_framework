30f4606335e9560933ad3e7e679754dbf4af009d
Andrew Cambridge <andrewbcambridge@gmail.com>
2024-03-22 08:00:29 -0600

    code cleanup

--- int_handle_testing.py
+++ int_handle_testing.py
@@ -4,39 +4,29 @@ import shutil
 from src.quest import step, create_filesystem_historian
 import sys
 
-sys.stderr = open('err.txt', 'w')
+sys.stderr = open('err.txt', 'w') # you'll want this file listed in your .gitignore file
 
 # global events to be set by the workflow and indicate how far the workflow progressed
 three_events: list[asyncio.Event] = [asyncio.Event(), asyncio.Event(), asyncio.Event()]
 
 # directory for storage of json files
-test_state = Path("test-state-(temp)")
+test_state = Path("saved-state")
 
 # broadly used constant and variables
-MAX_ITERATIONS = 3
+MAX_ITERATIONS = 4
 SUCCESSFULL_COMPLETION = "workflow completed without exception"
-
-class Trigger():
-    def __init__(self):
-        self.iteration_trigger = 0
-    
-    def get_trigger(self):
-        return self.iteration_trigger
-    
-    def set_trigger(self, new_val: int):
-        self.iteration_trigger = new_val
-
-ITERATION_TRIGGER: Trigger = Trigger()
+interrupt_trigger = 0
 
 @step
 async def throwExceptionOnTrigger(current_iteration):
-    if(current_iteration == ITERATION_TRIGGER.get_trigger()):
+    global interrupt_trigger
+    if(current_iteration == interrupt_trigger):
         raise KeyboardInterrupt
 
 async def createInterrupt():
     current_iteration: int = 1
 
-    while(current_iteration <= MAX_ITERATIONS):
+    while(current_iteration < MAX_ITERATIONS):
         three_events[current_iteration - 1].set()
         await throwExceptionOnTrigger(current_iteration)
         current_iteration += 1
@@ -62,8 +52,9 @@ def test_manager_background():
     # clean up files before the test
     shutil.rmtree(test_state, ignore_errors=True)
 
+    global interrupt_trigger
     iteration = 1
-    while(iteration <= MAX_ITERATIONS + 1):
+    while(iteration <= MAX_ITERATIONS):
         # reset the events
         for event in three_events:
             event.clear()
@@ -72,14 +63,15 @@ def test_manager_background():
         asyncio.set_event_loop(loop)
         
         try:
-            ITERATION_TRIGGER.set_trigger(iteration)
-            print(f"Iteration number {iteration}")
+            interrupt_trigger = iteration
+            print(f"Iteration {iteration}:")
             result = loop.run_until_complete(run_faulty_workflow())
 
         except Exception as ex:
             for i in range(iteration):
                 assert three_events[i].is_set()
 
+            print("PASSED\n")
             continue
 
         finally:
@@ -87,11 +79,12 @@ def test_manager_background():
             loop.close()
             iteration = iteration + 1
 
-        # no exceptions were thrown (4 was passed) all events should be set
+        # no exceptions were thrown (4 was passed in) all events should be set
         for event in three_events:
             assert event.is_set()
 
         assert result == SUCCESSFULL_COMPLETION
+        print("PASSED\n")
         continue
 
 if __name__ == '__main__': 
