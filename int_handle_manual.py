459c52c584d116fc428f79a90f2fd9807af47a72
Andrew Cambridge <andrewbcambridge@gmail.com>
2024-03-21 21:37:37 -0600

    auto throw of KeyboardInterrupt is working well, not sure what is different from the json issues

--- int_handle_manual.py
+++ int_handle_manual.py
@@ -2,21 +2,25 @@ import asyncio
 from pathlib import Path
 import shutil
 from src.quest import step, create_filesystem_historian
+import random
+import sys
 
-# try code similar to int_handle_testing, except that it just iterates which pauses inbetween, and then the user chooses when to throw the Ctrl+C interrupt. 
-    # Just loop, output what iteration you're on, then sleep for 5 seconds
+# this code is intended to run in such a way that it is easy for the developer to interrupt the program with Ctrl+C and observe how it resumes
 
 MAX_ITERATIONS = 10
+random_breakpoint = 0
 
 @step
 async def enter_loop(iteration: int):
+    if iteration == random_breakpoint:
+        raise KeyboardInterrupt
     await asyncio.sleep(0.5)
-    msg: str = f'Starting iteration {iteration} and sleeping for 3 seconds'
+    msg: str = f'Starting iteration {iteration} and sleeping for 1 seconds'
     return msg
 
 @step
 async def exit_loop(iteration: int):
-    await asyncio.sleep(3)
+    await asyncio.sleep(1)
     msg: str = f'Ending iteration {iteration} and sleeping for .5 seconds\n'
     return msg
 
@@ -31,7 +35,7 @@ async def run_iterations():
     await asyncio.sleep(5)
 
 async def run_workflow():
-    print("Running a max of 10 iterations:\n\n")
+    print("Running a max of 10 iterations:\n")
 
     historian = create_filesystem_historian(Path('saved-state'), 'manual-sigint-testing', run_iterations)
 
@@ -41,8 +45,18 @@ async def run_workflow():
     print("Workflow completed all 10 iterations successfully")
 
 if __name__ == '__main__':
+    print("\nSpecify \"-r\" in the argument to clear the history first")
+
+    args = sys.argv
+    if len(args) > 1 and args[1] == "-r":
+        shutil.rmtree("saved-state", ignore_errors=True)
+        print("json files removed.")
+
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
+
+    random_breakpoint = random.randint(0, 10) # selecting 10 means no interrup will be raised
+    print(f"Running with random_breakpont set to {random_breakpoint}")
 
     try:
         loop.run_until_complete(run_workflow())
