336c088faa113d36921b26a8efe0ed0785793c03
ABCambridge <andrewbcambridge@gmail.com>
2024-04-01 10:35:18 -0600

    temp.py is working and passing on Windows

--- temp.py
+++ temp.py
@@ -3,15 +3,19 @@ import subprocess
 import time
 import platform
 
-def get_subprocess():
-    p = subprocess.Popen("python ./main.py")
+def get_subprocess(is_fresh=False):
+    if(is_fresh): # delete json files on first run of main.py
+        p = subprocess.Popen("python ./main.py --no-delete")
+    else:
+        p = subprocess.Popen("python ./main.py")
     time.sleep(1)
     return p
 
 def check_fresh_run():
     print("--------Clean Test--------")
-    p = get_subprocess()
+    p = get_subprocess(is_fresh=True)
     p.wait()
+    print(p.returncode)
     assert p.returncode == 0
 
 def run_test():
@@ -27,6 +31,7 @@ def run_test():
     p = get_subprocess()
     p.kill()
     p.wait()
+    print(p.returncode)
     assert p.returncode == 1
 
     check_fresh_run()
