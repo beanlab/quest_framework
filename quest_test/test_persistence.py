92d33caee9b4e2b7b6c6bb8b70d8b91003d3554d
ABCambridge <andrewbcambridge@gmail.com>
2024-04-03 08:12:56 -0600

    duplicated tests to use both kinds of persistent history. all 4 test are passing

--- quest_test/test_persistence.py
+++ quest_test/test_persistence.py
@@ -5,7 +5,7 @@ import pytest
 
 from src.quest import step
 from src.quest.historian import Historian
-from src.quest.persistence import LinkedPersistentHistory, LocalFileSystemBlobStorage
+from src.quest.persistence import LinkedPersistentHistory, ListPersistentHistory, LocalFileSystemBlobStorage
 from quest_test.utils import timeout
 
 
@@ -26,7 +26,7 @@ async def simple_workflow():
 
 @pytest.mark.asyncio
 @timeout(3)
-async def test_persistence_basic(tmp_path: Path):
+async def test_persistence_basic_linked(tmp_path: Path):
     storage = LocalFileSystemBlobStorage(tmp_path)
     history = LinkedPersistentHistory('test', storage)
     historian = Historian(
@@ -49,9 +49,32 @@ async def test_persistence_basic(tmp_path: Path):
     result = await historian.run()
     assert result == 14
 
+@pytest.mark.asyncio
+@timeout(3)
+async def test_persistence_basic_list(tmp_path: Path):
+    storage = LocalFileSystemBlobStorage(tmp_path)
+    history = LinkedPersistentHistory('test', storage)
+    historian = Historian(
+        'test',
+        simple_workflow,
+        history
+    )
+
+    workflow = historian.run()
+    await asyncio.sleep(0.01)
+    await historian.suspend()
+
+    pause.set()
+    history = LinkedPersistentHistory('test', storage)
+    historian = Historian(
+        'test',
+        simple_workflow,
+        history
+    )
+    result = await historian.run()
+    assert result == 14
 
 event = asyncio.Event()
-
 
 @step
 async def blocks():
@@ -64,7 +87,45 @@ async def resume_this_workflow():
 
 
 @pytest.mark.asyncio
-async def test_resume_step_persistence(tmp_path: Path):
+async def test_resume_step_persistence_linked(tmp_path: Path):
+    storage = LocalFileSystemBlobStorage(tmp_path)
+    history = LinkedPersistentHistory('test', storage)
+    historian = Historian(
+        'test',
+        resume_this_workflow,
+        history
+    )
+
+    workflow = historian.run()
+    await asyncio.sleep(0.01)
+    await historian.suspend()
+
+    event.set()
+
+    storage = LocalFileSystemBlobStorage(tmp_path)
+    history = LinkedPersistentHistory('test', storage)
+    historian = Historian(
+        'test',
+        resume_this_workflow,
+        history
+    )
+
+    await historian.run()
+
+event = asyncio.Event()
+
+@step
+async def blocks():
+    await event.wait()
+    return 7
+
+
+async def resume_this_workflow():
+    await blocks()
+
+
+@pytest.mark.asyncio
+async def test_resume_step_persistence_list(tmp_path: Path):
     storage = LocalFileSystemBlobStorage(tmp_path)
     history = LinkedPersistentHistory('test', storage)
     historian = Historian(
