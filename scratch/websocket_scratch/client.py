af5ca66937cd09179bb9ee0be1be95f0c3f37f12
Kyle Standing <kjstanding2@gmail.com>
2024-09-24 21:58:26 -0600

    Implement first draft of websocket auth

--- scratch/websocket_scratch/client.py
+++ scratch/websocket_scratch/client.py
@@ -1,18 +1,32 @@
-#!/usr/bin/env python
-
 import asyncio
 import websockets
+import jwt
 
-async def hello():
+from quest import ainput
+
+SECRET_KEY = "C@n'tT0uchThis!"
+TOKEN = jwt.encode({"ident": "client123"}, SECRET_KEY, algorithm="HS256")
+
+async def send_messages(websocket):
+    while True:
+        message = await ainput("Enter message to send: ")
+        await websocket.send(message)
+
+async def receive_messages(websocket):
+    async for message in websocket:
+        print(f"Received message: {message}")
+
+async def main():
     uri = "ws://localhost:8765"
-    async with websockets.connect(uri) as websocket:
-        name = input("What's your name? ")
+    headers = {
+        "Authorization": f"Bearer {TOKEN}"
+    }
 
-        await websocket.send(name)
-        print(f">>> {name}")
-
-        greeting = await websocket.recv()
-        print(f"<<< {greeting}")
+    async with websockets.connect(uri, extra_headers=headers) as websocket:
+        await asyncio.gather(
+            send_messages(websocket),
+            receive_messages(websocket)
+        )
 
 if __name__ == "__main__":
-    asyncio.run(hello())
+    asyncio.run(main())
--- scratch/websocket_scratch/server.py
+++ scratch/websocket_scratch/server.py
@@ -2,19 +2,57 @@
 
 import asyncio
 import websockets
+import jwt
 
-async def hello(websocket):
-    name = await websocket.recv()
-    print(f"<<< {name}")
+from jwt.exceptions import InvalidTokenError
+from src.quest import WorkflowManager
 
-    greeting = f"Hello {name}!"
+SECRET_KEY = "C@n'tT0uchThis!"
 
-    await websocket.send(greeting)
-    print(f">>> {greeting}")
+async def receive_messages(manager: WorkflowManager, ident, websocket, path):
+    async for message in websocket:
+        print("Received message: ", message)
+        # await manager.send_event(ident, message)
 
-async def main():
-    async with websockets.serve(hello, "localhost", 8765):
-        await asyncio.Future()  # run forever
+async def send_messages(manager, ident, websocket, path):
+    # How exactly are we calling stream_resources on the manager?
+    async for status in manager.stream_resources(ident):
+        websocket.send(status)
+
+async def authorize(websocket):
+    try:
+        token = websocket.request_headers['Authorization']
+        if token.startswith("Bearer "):
+            token = token.split(" ")[1]
+        else:
+            return False
+
+        decoded_token = jwt.decode(token, SECRET_KEY, algorithms="HS256")
+        return decoded_token
+    except (KeyError, InvalidTokenError):
+        return False
+
+async def default_handler(manager, websocket, path):
+    authorized = await authorize(websocket)
+    if not authorized:
+        await websocket.close(code=websocket.CLOSE_STATUS_POLICY_VIOLATION, reason="Unauthorized")
+        return
+
+    ident = authorized['ident']
+
+    await asyncio.gather(
+        receive_messages(manager, ident, websocket, path),
+        send_messages(manager, ident, websocket, path)
+    )
+
+async def serve(manager, host, port):
+    # Start the WebSocket server
+    async with websockets.serve(lambda ws, p: default_handler(manager, ws, p), host, port):
+        await asyncio.Future()
+
+async def initialize(namespace, storage, create_history, workflow):
+    async with WorkflowManager(namespace, storage, create_history, lambda w_type: workflow) as manager:
+        await serve(manager, "localhost", 8765)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(initialize())
