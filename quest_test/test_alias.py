db3dc87a6917f3c875b981a069a1a681dc7ced7e
cwebbles <94764100+cwebbles@users.noreply.github.com>
2024-10-04 18:31:41 -0600

    Refactor test_alias

--- quest_test/test_alias.py
+++ quest_test/test_alias.py
@@ -1,6 +1,10 @@
 import asyncio
 import pytest
+import logging
 
+from quest import WorkflowManager
+from quest.manager import workflow_manager
+from quest.persistence import InMemoryBlobStorage, PersistentHistory
 from src.quest import step, queue
 from src.quest.historian import Historian
 from src.quest.wrappers import task
@@ -11,36 +15,62 @@ from utils import timeout
 
 @pytest.mark.asyncio
 async def test_alias():
-    gate_a = asyncio.Event()
-    gate_b = asyncio.Event()
+    pause = asyncio.Event()
+    data_a = []
+    data_b = []
 
-    @task
     async def workflow_a():
-        data = []
         async with queue('data', None) as q:
             async with alias('the_foo'):
-                data.append(await q.get())
-            await gate_a.wait()
-            data.append(await q.get())
+                data_a.append(await q.get())
+            await pause.wait()
+            data_a.append(await q.get())
 
-    @task
     async def workflow_b():
         # TODO: How should I check the data?
-        data = []
         async with queue('data', None) as q:
-            await gate_b.wait()
+            await pause.wait()
             async with alias('the_foo'):
-                data.append(await q.get())
-            data.append(await q.get())
-        return data
+                data_b.append(await q.get())
+            data_b.append(await q.get())
 
-    async def run_workflows():
-        # TODO: Is this how I should be switching off alias?
-        res_a = await workflow_a()
-        res_b = await workflow_b()
+    storage = InMemoryBlobStorage()
+    histories = {}
 
-        print(res_a)
-        print(res_b)
+    def create_history(wid: str):
+        if wid not in histories:
+            histories[wid] = PersistentHistory(wid, InMemoryBlobStorage())
+        return histories[wid]
+
+    async with WorkflowManager('test_alias', storage, create_history, lambda w_type: workflow_a) as manager:
+        manager.start_workflow('workflow_a', 'wid_a')
+        foo_resources = await manager.get_resources('the_foo', None)
+        a_resources = await manager.get_resources('wid_a', None)
+        # TODO: Will there be two different queues here?
+        data_foo = foo_resources['data']
+        data_a = a_resources['data']
+        await data_a.put('I am Workflow A')
+        await data_foo.put('I am the FOO')
+
+    async with WorkflowManager('test_alias', storage, create_history, lambda w_type: workflow_b) as manager:
+        manager.start_workflow('workflow_b', 'wid_b')
+        resources = await manager.get_resources('the_foo', None)
+        data_foo = resources['data']
+        # Now pause the manager and all workflows
+
+    assert 'wid1' in histories
+    assert counter_a == 1
+    assert counter_b == 0
+
+    async with WorkflowManager('test-manager', storage, create_history, lambda w_type: workflow) as manager:
+        # At this point, all workflows should be resumed
+        pause.set()
+        await asyncio.sleep(0.1)
+        result = await manager.get_workflow('wid1')
+        assert result == 11
+
+    assert counter_a == 2
+    assert counter_b == 1
 
     history = []
     # TODO: Does historian wrap the workflow stuff?
