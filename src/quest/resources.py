d201badea9d0c383c991144678848b8362c0d749
Kyle Standing <kjstanding2@gmail.com>
2024-10-09 10:36:07 -0600

    Add change to on_close callback

--- src/quest/resources.py
+++ src/quest/resources.py
@@ -7,6 +7,18 @@ class ResourceStreamManager:
     def __init__(self):
         self.resource_streams: dict[str, set[ResourceStreamManager.ResourceStream]] = {}
 
+    class ResourceStreamContext:
+        def __init__(self, on_close: Callable):
+            self._on_close = on_close
+
+        def __enter__(self):
+            logging.debug(f'Resource stream opened for {id(self)}')
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            self._on_close()
+            logging.debug(f'Resource stream closed for {id(self)}')
+
     class ResourceStream:
         def __init__(self,
                      get_resources: Callable[[], Coroutine],
@@ -18,17 +30,6 @@ class ResourceStreamManager:
             self._get_resources = get_resources
             self._is_workflow_complete = is_workflow_complete
             self._on_close = on_close
-            self._is_entered = False
-            # TODO - set any gates?
-
-        async def __aenter__(self):
-            self._is_entered = True
-            logging.debug(f'Resource stream opened for {id(self)}')
-            return self
-
-        async def __aexit__(self, exc_type, exc_value, traceback):
-            self._on_close(self)
-            logging.debug(f'Resource stream closed for {id(self)}')
 
         async def __aiter__(self):
             """
@@ -40,21 +41,19 @@ class ResourceStreamManager:
             Use wait_for_completion() to automatically iterate (and ignore) the remaining updates.
             """
 
-            if not self._is_entered:
-                raise Exception('ResourceStream must be used in a `with` context.')
-
             # The caller of this function lives outside the step management of the historian
             #         -> don't replay, just yield event changes in realtime
 
-            # Yield the current resources immediately
-            yield await self._get_resources()
-
-            while not self._is_workflow_complete():
-                # Yield new resources updates as they become available
-                await self._update_event.wait()
-                self._update_event.clear()
+            with ResourceStreamManager.ResourceStreamContext(lambda: self._on_close(self)):
+                # Yield the current resources immediately
                 yield await self._get_resources()
-                self._stream_gate.set()
+
+                while not self._is_workflow_complete():
+                    # Yield new resources updates as they become available
+                    await self._update_event.wait()
+                    self._update_event.clear()
+                    yield await self._get_resources()
+                    self._stream_gate.set()
 
     def get_resource_stream(self,
                             identity,
@@ -64,7 +63,7 @@ class ResourceStreamManager:
         rs = ResourceStreamManager.ResourceStream(
             get_resources,
             is_workflow_complete,
-            lambda res_stream: self.resource_streams[identity].remove(res_stream)
+            lambda res_stream: self._on_close(identity, res_stream)
         )
         if identity not in self.resource_streams:
             self.resource_streams[identity] = set()
@@ -82,3 +81,9 @@ class ResourceStreamManager:
             stream._update_event.set()
             await stream._stream_gate.wait()
             stream._stream_gate.clear()
+
+    def _on_close(self, identity, res_stream: ResourceStream):
+        self.resource_streams[identity].remove(res_stream)
+        rs_set = self.resource_streams.get(identity)
+        if len(rs_set) == 0:
+            self.resource_streams.pop(identity)
