60d079d30e789e44490de2db4a4d3c4901e70a19
niclew17 <niclewis17@gmail.com>
2024-10-21 08:40:56 -0600

    Changed the imports to src.quest and made the global variables different

--- quest_test/test_get_exception_class.py
+++ quest_test/test_get_exception_class.py
@@ -1,7 +1,7 @@
 import pytest
 
 from quest_test.custom_errors.custom_error import MyError
-from src.quest.historian import get_exception_class
+from src.quest.historian import _get_exception_class
 
 
 class CustomExceptionOne(Exception):
@@ -13,26 +13,26 @@ class CustomExceptionTwo(Exception):
 
 
 def test_standard_exceptions():
-    assert get_exception_class('builtins.ValueError') == ValueError
-    assert get_exception_class('builtins.KeyError') == KeyError
-    assert get_exception_class('builtins.IndexError') == IndexError
-    assert get_exception_class('builtins.BufferError') == BufferError
-    assert get_exception_class('builtins.MemoryError') == MemoryError
-    assert get_exception_class('builtins.OverflowError') == OverflowError
-    assert get_exception_class('builtins.RecursionError') == RecursionError
-    assert get_exception_class('builtins.RuntimeError') == RuntimeError
+    assert _get_exception_class('builtins.ValueError') == ValueError
+    assert _get_exception_class('builtins.KeyError') == KeyError
+    assert _get_exception_class('builtins.IndexError') == IndexError
+    assert _get_exception_class('builtins.BufferError') == BufferError
+    assert _get_exception_class('builtins.MemoryError') == MemoryError
+    assert _get_exception_class('builtins.OverflowError') == OverflowError
+    assert _get_exception_class('builtins.RecursionError') == RecursionError
+    assert _get_exception_class('builtins.RuntimeError') == RuntimeError
 
 
 def test_custom_exceptions():
-    exception_class = get_exception_class('quest_test.custom_errors.custom_error.MyError')
+    exception_class = _get_exception_class('quest_test.custom_errors.custom_error.MyError')
     with pytest.raises(exception_class) as exc_info:
         raise exception_class("This is a custom error message")
     assert str(exc_info.value) == "This is a custom error message"
 
 
 def test_weird_custom_exception():
-    exception_class = get_exception_class('quest_test.custom_errors.custom_error.WeirdCustomException')
+    exception_class = _get_exception_class('quest_test.custom_errors.custom_error.WeirdCustomException')
     with pytest.raises(exception_class) as exc_info:
         raise exception_class("This is a weird custom error", 404)
-        assert str(exc_info.value) == "This is a weird custom error (Error Code: 404)"
-        assert exc_info.value.get_code() == 404
+    assert str(exc_info.value) == "This is a weird custom error (Error Code: 404)"
+    assert exc_info.value.get_code() == 404
