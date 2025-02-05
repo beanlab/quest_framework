c3dd796137ddb43af60eefe0cbcebd1465d5a3a0
Collin Webb <cwebbles34@gmail.com>
2025-02-04 20:04:21 -0700

    ainput working, error in guessing game

--- src/quest/utils.py
+++ src/quest/utils.py
@@ -2,12 +2,65 @@ import asyncio
 import logging
 from contextvars import ContextVar
 import sys
+import os
+import io
 
 task_name_getter = ContextVar("task_name_getter", default=lambda : "-")
 
-async def ainput(*args):
-    return await asyncio.to_thread(input, *args)
+async def stdio(loop=None):
+    if loop is None:
+        loop = asyncio.get_event_loop()
 
+    if sys.platform == "win32":
+        return _win32_stdio(loop)
+
+    reader = asyncio.StreamReader()
+    protocol = asyncio.StreamReaderProtocol(reader, loop=loop)
+
+    read_pipe = os.fdopen(sys.stdin.fileno(), "rb", buffering=0)
+
+    await loop.connect_read_pipe(lambda: protocol, read_pipe)
+
+    return reader
+
+def _win32_stdio(loop):
+    class Win32StdinReader:
+        def __init__(self):
+            self.stdin = sys.stdin.buffer
+
+        async def readline(self):
+            return await loop.run_in_executor(None, sys.stdin.readline)
+
+    return Win32StdinReader()
+
+
+async def ainput(prompt: str, reader=None):
+    """
+    Reads the stream until the end of the current content
+    Stops waiting for content after `timeout` seconds
+    Returns decoded content (i.e. str not bytes)
+    """
+    print(prompt, end="", flush=True)
+
+    if reader is None:
+        reader = await stdio()
+
+    line = await reader.readline()
+    return line.decode()
+
+# async def read_from_stdin(reader):
+#     buffer = []
+#     while True:
+#         token = await reader.read(1)
+#         if not token:
+#             # stream.read() returns an empty byte when EOF is reached
+#             break
+#         if token == '\n':
+#             return ''.join(buffer)
+#         token = token.decode()
+#         buffer.append(token)
+#
+#         await asyncio.sleep(0)
 
 class TaskFieldFilter(logging.Filter):
     def filter(self, record):
