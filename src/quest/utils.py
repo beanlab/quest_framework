d34aabc34dfef4e87decc9a4cb0239663a0461fb
Gordon Bean <gbean@cs.byu.edu>
2025-02-14 12:33:09 -0700

    format utils.py

--- src/quest/utils.py
+++ src/quest/utils.py
@@ -1,9 +1,9 @@
 import asyncio
 import logging
 from contextvars import ContextVar
-import sys
 
-task_name_getter = ContextVar("task_name_getter", default=lambda : "-")
+task_name_getter = ContextVar("task_name_getter", default=lambda: "-")
+
 
 async def ainput(*args):
     return await asyncio.to_thread(input, *args)
@@ -13,6 +13,7 @@ class TaskFieldFilter(logging.Filter):
     def filter(self, record):
         record.task = task_name_getter.get()()
         return True
+
 
 logging.getLogger().addFilter(TaskFieldFilter())
 quest_logger = logging.getLogger('quest')
