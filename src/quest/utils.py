5c7ee171d785c4e1aabb599074923dabd4ef7c65
Collin Webb <cwebbles34@gmail.com>
2025-02-19 10:15:36 -0700

    ainput simple

--- src/quest/utils.py
+++ src/quest/utils.py
@@ -7,29 +7,14 @@ from contextvars import ContextVar
 task_name_getter = ContextVar("task_name_getter", default=lambda: "-")
 
 import asyncio
-import sys
 
-async def stdio():
-    if sys.platform == "win32":
-        raise NotImplementedError()
-
-    reader = asyncio.StreamReader()
-    protocol = asyncio.StreamReaderProtocol(reader)
-
-    await asyncio.get_running_loop().connect_read_pipe(lambda: protocol, sys.stdin.buffer)
-
-    return reader
-
-async def ainput(prompt: str, reader=None):
-    print(prompt, end="", flush=True)
-    reader = reader or await stdio()
-    return (await reader.readline()).decode().strip()
+async def ainput(*args):
+    return await asyncio.to_thread(input, *args)
 
 class TaskFieldFilter(logging.Filter):
     def filter(self, record):
         record.task = task_name_getter.get()()
         return True
-
 
 logging.getLogger().addFilter(TaskFieldFilter())
 quest_logger = logging.getLogger('quest')
