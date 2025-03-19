05c47da72a31ff77cb42a292683639ffd962a5f1
YoungWoo Song <songwill226@gmail.com>
2025-03-18 22:54:17 -0600

    test_interruptions.py modified

--- quest_test/test_interruptions.py
+++ quest_test/test_interruptions.py
@@ -5,35 +5,29 @@ from asyncio import CancelledError
 
 import pytest
 from quest_test.utils import create_in_memory_workflow_manager
-from quest.manager import find_workflow_manager
+
+gate_1 = asyncio.Event()
+gate_2 = asyncio.Event()
 
 
 @pytest.mark.integration
 @pytest.mark.asyncio
 async def test_sigint_handling():
-    async def workflow_1(counter_1, gate_1_id, gate_2_id):
-        manager = find_workflow_manager()
-        gate_1 = await manager.get_event("w1", gate_1_id, None)
-        gate_2 = await manager.get_event("w1", gate_2_id, None)
-
+    async def workflow_1(counter_1, gate_1, gate_2):
         for i in range(1, 5):
             await gate_1.wait()
             counter_1[0] += 1
-            await gate_1.clear()
-            await gate_2.set()
+            gate_1.clear()
+            gate_2.set()
 
-    async def workflow_2(counter_2, gate_1_id, gate_2_id):
-        manager = find_workflow_manager()
-        gate_1 = await manager.get_event("w1", gate_1_id, None)
-        gate_2 = await manager.get_event("w1", gate_2_id, None)
-
+    async def workflow_2(counter_2, gate_1, gate_2):
         for i in range(1, 5):
             await gate_2.wait()
             counter_2[0] += 1
-            await gate_2.clear()
+            gate_2.clear()
             if i == 3:
                 os.kill(os.getpid(), signal.SIGINT)
-            await gate_1.set()
+            gate_1.set()
 
     workflows = {
         'workflow_1': workflow_1,
@@ -45,11 +39,8 @@ async def test_sigint_handling():
     counter_2 = [0]
 
     async with manager:
-        manager.start_workflow('workflow_1', 'w1', counter_1, 'gate_1', 'gate_2', delete_on_finish=False)
-        manager.start_workflow('workflow_2', 'w2', counter_2, 'gate_1', 'gate_2', delete_on_finish=False)
-
-        gate_1 = await manager.get_event("w1", 'gate_1', None)
-        gate_2 = await manager.get_event("w1", 'gate_2', None)
+        manager.start_workflow('workflow_1', 'w1', counter_1, delete_on_finish=False)
+        manager.start_workflow('workflow_2', 'w2', counter_2, delete_on_finish=False)
 
         gate_1.set()
 
