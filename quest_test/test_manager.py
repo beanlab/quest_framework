46d527067bd088618939282260357142f5e1f9eb
YoungWoo Song <songwill226@gmail.com>
2025-03-24 10:15:15 -0600

    test_manager.py modified

--- quest_test/test_manager.py
+++ quest_test/test_manager.py
@@ -49,7 +49,7 @@ async def test_manager():
         # At this point, all workflows should be resumed
         pause.set()
         await asyncio.sleep(0.1)
-        result = await manager.get_workflow_result('wid1', delete=False)
+        result = await manager.get_workflow_result('wid1')
         assert result == 11
 
     assert counter_a == 2
