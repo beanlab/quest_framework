5715f7cfc202bda88ef31b298a965b0ebba0790e
Collin Webb <cwebbles34@gmail.com>
2025-04-09 10:00:25 -0600

    simple demo

--- demos/simple_client.py
+++ demos/simple_client.py
@@ -1,19 +1,26 @@
 import asyncio
-from quest import Client
+from quest import Client, ainput
 
 async def render(resources):
-    if 'message' in resources:
-        message = as_state(resources['message'])
+    print('Rendering resources')
+    if ('message', None) in resources:
+        message = resources[('message', None)]
         print(message.get())
 
-    if 'response' in resources:
-
-
 async def main():
-    async with Client('localhost', '1234') as client:
-        await client.start_workflow('workflow', 'wid')
-        async for resources in client.stream_resources('wid', None):
-            await render(resources)
+    wid = 'wid'
+    async with Client('ws://localhost:8800', {'Foo': 'Bar'}) as client:
+        print('Client starting workflow')
+        await client.start_workflow('workflow', wid)
+        print('Client started workflow')
+        async for resources in client.stream_resources(wid, None):
+            print('Client received resource event')
+            if ('message', None) in resources:
+                message = await client.send_event(wid, 'message', None, 'get')
+                print(message)
+            if ('response', None) in resources:
+                user_response = await ainput()
+                await client.send_event(wid, 'response', None, 'put', user_response)
 
 
 if __name__ == '__main__':
--- demos/simple_server.py
+++ demos/simple_server.py
@@ -1,23 +1,31 @@
 import asyncio
+from pathlib import Path
 
 from quest import Server, create_filesystem_manager, state, queue
-from test_basic import workflow
 
+def authorizer(headers: dict[str, str]) -> bool:
+    return True
 
 async def workflow():
-    async with state('message', None, '') as message            :
-        await state.set('Enter a number: ')
+    async with state('message', None, 'Test') as message:
+        while True:
+            print('Setting message')
+            await message.set('Enter a number: ')
+            print('State set')
 
-        async with queue('response', None) as response:
-            resp = await response.get()
+            async with queue('response', None) as response:
+                resp = await response.get()
+                print('Response received: ' + str(resp))
 
-        await state.set('You said: ' + resp)
+            print('Setting message with user response')
+            await message.set('You said: ' + resp)
+
 
 
 async def main():
-    async with create_filesystem_manager('state', 'simple', {
-        'workflow': workflow
-    }) as manager, Server(manager, 'localhost', '1234'):
+    state_folder = Path('state')
+    async with create_filesystem_manager(state_folder, 'simple', lambda wid: workflow) as manager, Server(manager, 'localhost', 8800, authorizer):
+        print("Server started at ws://localhost:8800")
         await asyncio.Future()
 
 
